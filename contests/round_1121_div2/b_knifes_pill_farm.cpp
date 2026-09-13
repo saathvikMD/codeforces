ae1ed1e5f3c7df6241c74274859d64df388f8c24
saathvikMD <65287434+saathvikMD@users.noreply.github.com>
2026-09-14 00:58:42 +0530

    Editing solution for Knife's Pill Farm

--- contests/round_1121_div2/b_knifes_pill_farm.cpp
+++ contests/round_1121_div2/b_knifes_pill_farm.cpp
@@ -15,36 +15,31 @@ int main() {
         cin >> n >> m;
 
         vector<long long> numbers(n);
-        vector<long long> original_numbers(n);
 
         for (int i = 0; i < n; i++) {
             cin >> numbers[i];
-
-            original_numbers[i] = numbers[i];
         }
-
-        sort(numbers.begin(), numbers.end());
 
         long long m_sum = 0;
         priority_queue<long long> queue;
 
         for (int i = 0; i < m - 1; i++) {
-            queue.push(original_numbers[i]);
-            m_sum += original_numbers[i];
+            queue.push(numbers[i]);
+            m_sum += numbers[i];
         }
 
         long long max_count = LLONG_MIN;
 
         for (int i = m - 1; i < n; i++) {
-            if (m > 1 and i >= m and original_numbers[i - 1] < queue.top()) {
+            if (m > 1 and i >= m and numbers[i - 1] < queue.top()) {
                 m_sum -= queue.top();
                 queue.pop();
 
-                queue.push(original_numbers[i - 1]);
-                m_sum += original_numbers[i - 1];
+                queue.push(numbers[i - 1]);
+                m_sum += numbers[i - 1];
             }
 
-            long long curr_count = original_numbers[i] * m - m_sum;
+            long long curr_count = numbers[i] * m - m_sum;
             max_count = max(max_count, curr_count);
         }
 
